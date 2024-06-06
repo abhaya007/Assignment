89a7d9162f8624ff4464684ae0486c65c73561a7
Abhaya N K <abhayakhatri48@gmail.com>
2024-06-06 16:52:45 +0545

    ObjAssg1

---
+++ Javascript/Object/level1.js
@@ -0,0 +1,6 @@
+// Q1: Retrieve the value of the 'model' property from the 'car' object.
+// Expected output:
+// 'Civic'
+const car1 = { make: 'Honda', model: 'Civic', year: 2020 };
+const value= car1['model'];
+console.log(value);
