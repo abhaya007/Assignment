2120def27f35467668b23c0c4ba9e378983f57a9
Abhaya N K <abhayakhatri48@gmail.com>
2024-06-06 17:38:01 +0545

    Objlevel2

--- Javascript/Object/level2.js
+++ Javascript/Object/level2.js
@@ -2,5 +2,11 @@
 // Expected output:
 // 'John Doe'
 const person1 = { firstName: 'John', lastName: 'Doe', age: 30 };
-console.log(person1.firstName+' '+person1.lastName);
+console.log(person1.firstName+' '+person1.lastName)
 
+// Q2: Create a new object by merging the 'details1' and 'details2' objects.
+// Expected output:
+// { name: 'John', age: 25, city: 'New York', hobby: 'Soccer' }
+const details1 = { name: 'John', age: 25 };
+const details2 = { city: 'New York', hobby: 'Soccer' };
+console.log(Object.assign(details1,details2));
