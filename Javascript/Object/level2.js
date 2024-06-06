59c3c46f230a19c0ff73a797ea12d30631195e26
Abhaya N K <abhayakhatri48@gmail.com>
2024-06-06 17:34:42 +0545

    Objlevel2

---
+++ Javascript/Object/level2.js
@@ -0,0 +1,6 @@
+// Q1: Extract and return the 'firstName' and 'lastName' properties as a single string from the 'person' object.
+// Expected output:
+// 'John Doe'
+const person1 = { firstName: 'John', lastName: 'Doe', age: 30 };
+console.log(person1.firstName+' '+person1.lastName);
+
