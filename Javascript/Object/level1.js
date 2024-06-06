bb724b704ac746bfbc901678d31096448f7af9c7
Abhaya N K <abhayakhatri48@gmail.com>
2024-06-06 16:56:13 +0545

    ObjAssg1

--- Javascript/Object/level1.js
+++ Javascript/Object/level1.js
@@ -4,3 +4,10 @@
 const car1 = { make: 'Honda', model: 'Civic', year: 2020 };
 const value= car1['model'];
 console.log(value);
+
+// Q2: Add a new property 'color' with the value 'red' to the 'shirt' object.
+// Expected output:
+// { size: 'large', material: 'cotton', color: 'red' }
+const shirt2 = { size: 'large', material: 'cotton' };
+const shirt = {size: 'large', material: 'cotton', color: 'red'};
+console.log(shirt);
