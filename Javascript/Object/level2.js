9acdd84029875a4ea431c5a22188d1eeebe0c948
Abhaya NK <161853968+abhaya007@users.noreply.github.com>
2024-06-07 11:03:10 +0545

    Objlvl2

--- Javascript/Object/level2.js
+++ Javascript/Object/level2.js
@@ -7,29 +7,32 @@ console.log(person1.firstName+' '+person1.lastName)
 // Q2: Create a new object by merging the 'details1' and 'details2' objects.
 // Expected output:
 // { name: 'John', age: 25, city: 'New York', hobby: 'Soccer' }
-const details1 = { name: 'John', age: 25 };
+ const details1 = { name: 'John', age: 25 };
 const details2 = { city: 'New York', hobby: 'Soccer' };
-console.log(Object.assign(details1,details2));
+const merged = {...details1, ...details2};
+console.log(merged);
 
 // Q3: Add a new property 'quantity' with the value 10 to the 'product' object, if it doesn't already exist.
 // Expected output:
 // { name: 'Laptop', price: 800, quantity: 10 }
 const product3 = { name: 'Laptop', price: 800 };
-product3.quantity = 10;
+if(!product3.quantity){  
+  product3.quantity = 10;
+}
 console.log(product3);
 
 // Q4: Check if all properties ('make', 'model', 'year') exist in the 'car' object.
 // Expected output:
 // false
 const car4 = { make: 'Toyota', year: 2021 };
-const checkAllProperties = car4.hasOwnProperty('make')&&car4.hasOwnProperty('model')&&car4.hasOwnProperty('year')
-console.log(checkAllProperties);
+checkProp = 'make' in car4 && 'model' in car4 && 'year' in car4;
+console.log(checkProp);
 
 // Q5: Create a copy of the 'student' object without modifying the original object.
 // Expected output:
 // { name: 'Alice', age: 20, grade: 'A' }
 const student5 = { name: 'Alice', age: 20, grade: 'A' };
-const copy = student5;
+const copy = {...student5};
 console.log(copy);
 
 // Q6: Check if the 'address' property in the 'user' object is a string.
@@ -59,7 +62,7 @@ console.log(Object.values(data8));
 // Expected output:
 // false
 const book9 = { title: 'JavaScript Basics', author: 'John Smith', year: 2020 };
-if(Object.values(book9) === 'null')
+if(Object.values(book9).includes(null)
   {
     conslole.log(true);
   }
